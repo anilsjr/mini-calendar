3e088258a7421f09c03d64a6d1320cbdebd3cf8b
Anil Vishwakarma <anil.vishwakarma.it@gmail.com>
2025-03-02 20:52:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const day = document.getElementById('day');
 const date = document.getElementById('date');
 
 const monthName = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-const dayName = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+const dayName = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
 window.onload = function() {
   month.innerHTML = monthName[d.getMonth()];
