6de6dfba920e2d349db65c4378f1715066aaa102
Anil Vishwakarma <anil.vishwakarma.it@gmail.com>
2025-03-02 20:47:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
+
+
+const d = new Date();
 const month = document.getElementById('month');
 const year = document.getElementById('year');
 const day = document.getElementById('day');
 const date = document.getElementById('date');
 
-const monthName =["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-const dayName =["Monday", "Tuesday", "Wednesday", "Thusday", "Friday", "Satuarday", "Sunday"];
-
+const monthName = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+const dayName = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
 
 const d = new Date();
 
-
-
-month.innerHTML = monthName[d.getMonth()];
-year.innerHTML = d.getFullYear();
-date.innerHTML = d.getDate();
-day.innerHTML = dayName[d.getDay()];
-
-
-
+window.onload = function() {
+  month.innerHTML = monthName[d.getMonth()];
+  year.innerHTML = d.getFullYear();
+  date.innerHTML = d.getDate();
+  day.innerHTML = dayName[d.getDay()];
+};
